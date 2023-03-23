68b55d2a65f65498dcdd97c6f287a72181072dbd
SoleOrtiz <115570349+SoleOrtiz@users.noreply.github.com>
2023-03-23 01:44:52 -0300

    Ejercicio grupal validacion

--- 3_JS/Clase_7/script/script.js
+++ 3_JS/Clase_7/script/script.js
@@ -1,3 +1,77 @@
+function validarFormulario() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let edad = document.getElementById("edad").value;
+    let dni = document.getElementById("dni").value;
+    let email = document.getElementById("email").value;
+  
+    if (nombre.length < 2 || nombre.length > 20) {
+        alert("El nombre debe tener entre 2 y 20 caracteres.");
+        return false;
+      }
+    
+      if (apellido.length < 2 || apellido.length > 25) {
+        alert("El apellido debe tener entre 2 y 25 caracteres.");
+        return false;
+      }
+    
+      if (edad < 18 || edad > 120 ) {
+        alert("La edad debe ser un número entre 18 y 120.");
+        return false;
+      }
+    
+      if (dni.length <= 6) {
+        alert("El DNI debe tener más de 6 caracteres.");
+        return false;
+      }
+    
+      if (!email.includes("@")) {
+        alert("El correo electrónico debe incluir '@'.");
+        return false;
+      }
+
+      //document.getElementById("nuevatabla").innerHTML= "<th>" + nombre + "</th>" nombre +"</th>" ;
+    
+    
+    document.getElementById("tablaNombre").innerHTML=nombre;
+    document.getElementById("tablaApellido").innerHTML= apellido;
+    document.getElementById("tablaEdad").innerHTML= edad;
+    document.getElementById("tablaDni").innerHTML= dni;
+    document.getElementById("tablaEmail").innerHTML= email;
+
+}
+
+    
+    /*agregarDatosTabla(nombre, apellido, edad, dni, email);
+  
+    document.getElementById("nombre").value = "";
+    document.getElementById("apellido").value = "";
+    document.getElementById("edad").value = "";
+    document.getElementById("dni").value = "";
+    document.getElementById("email").value = "";
+    
+  
+ 
+  
+  /*
+  function agregarDatosTabla(nombre, apellido, edad, dni, email) {
+    var tabla = document.querySelector("table");
+    var fila = tabla.insertRow();
+    var celdaNombre = fila.insertCell();
+    var celdaApellido = fila.insertCell();
+    var celdaEdad = fila.insertCell();
+    var celdaDni = fila.insertCell();
+    var celdaEmail = fila.insertCell();
+  
+    celdaNombre.innerText = nombre;
+    celdaApellido.innerText = apellido;
+    celdaEdad.innerText = edad;
+    celdaDni.innerText = dni;
+    celdaEmail.innerText = email;
+  }
+*/
+
+
 /*document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("formulario").addEventListener('submit', validarFormulario); 
   });
@@ -30,69 +104,5 @@
       return;
     }
     this.submit();
-  }
-*/
-
-function validarFormulario() {
-    let nombre = document.getElementById("nombre").value;
-    let apellido = document.getElementById("apellido").value;
-    let edad = document.getElementById("edad").value;
-    let dni = document.getElementById("dni").value;
-    let email = document.getElementById("email").value;
-  
-    if (nombre.length < 2 || nombre.length > 20) {
-        alert("El nombre debe tener entre 2 y 20 caracteres.");
-        return false;
-      }
-    
-      if (apellido.length < 2 || apellido.length > 25) {
-        alert("El apellido debe tener entre 2 y 25 caracteres.");
-        return false;
-      }
-    
-      if (edad < 18 || edad > 120 ) {
-        alert("La edad debe ser un número entre 18 y 120.");
-        return false;
-      }
-    
-      if (dni.length <= 6) {
-        alert("El DNI debe tener más de 6 caracteres.");
-        return false;
-      }
-    
-      if (!email.includes("@")) {
-        alert("El correo electrónico debe incluir '@'.");
-        return false;
-      }
-      
-}
-
-    /*
-    agregarDatosTabla(nombre, apellido, edad, dni, email);
-  
-    document.getElementById("nombre").value = "";
-    document.getElementById("apellido").value = "";
-    document.getElementById("edad").value = "";
-    document.getElementById("dni").value = "";
-    document.getElementById("email").value = "";
-    */
-  
- 
-  
-  /*
-  function agregarDatosTabla(nombre, apellido, edad, dni, email) {
-    var tabla = document.querySelector("table");
-    var fila = tabla.insertRow();
-    var celdaNombre = fila.insertCell();
-    var celdaApellido = fila.insertCell();
-    var celdaEdad = fila.insertCell();
-    var celdaDni = fila.insertCell();
-    var celdaEmail = fila.insertCell();
-  
-    celdaNombre.innerText = nombre;
-    celdaApellido.innerText = apellido;
-    celdaEdad.innerText = edad;
-    celdaDni.innerText = dni;
-    celdaEmail.innerText = email;
   }
 */
